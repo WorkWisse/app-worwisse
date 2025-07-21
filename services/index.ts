f6230ac3715e192ce08cfcac0360ac6fc32571d3
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2025-07-21 19:11:26 -0300

    feat: :fire: add logo and multiple changes

--- services/index.ts
+++ services/index.ts
@@ -1,4 +1,5 @@
 export { CompanyService } from "./companyService";
 export { ReviewService } from "./reviewService";
 export { SearchService } from "./searchService";
+export { ImageService } from "./imageService";
 export type { SearchSuggestion } from "./searchService";
