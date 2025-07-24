5164fe83e8c58c70a89b904fd3a9f493776a9da3
Nico Gilardoni <nicogilardonik@gmail.com>
2025-07-24 15:52:13 -0300

    refactor: Comment out LandingFeaturedLogos component in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import DefaultLayout from "@/layouts/default";
 import { LandingHero } from "@/modules/landing/components/LandingHero";
-import { LandingFeaturedLogos } from "@/modules/landing/components/LandingFeaturedLogos";
+//import { LandingFeaturedLogos } from "@/modules/landing/components/LandingFeaturedLogos";
 import { LandingFeatures } from "@/modules/landing/components/LandingFeatures";
 import { LandingMainContent } from "@/modules/landing/components/LandingMainContent";
 import { LandingFAQ } from "@/modules/landing/components/LandingFAQ";
@@ -9,7 +9,7 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <LandingHero />
-      <LandingFeaturedLogos />
+      {/* <LandingFeaturedLogos /> */}
       <LandingFeatures />
       <LandingMainContent />
       <LandingFAQ />
