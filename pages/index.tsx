0d6610b7c8375b36979af0d514e5df758dff7237
Nico Gilardoni <nicogilardonik@gmail.com>
2025-07-24 15:53:14 -0300

    refactor: Remove LandingFeaturedLogos component from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import DefaultLayout from "@/layouts/default";
 import { LandingHero } from "@/modules/landing/components/LandingHero";
-//import { LandingFeaturedLogos } from "@/modules/landing/components/LandingFeaturedLogos";
 import { LandingFeatures } from "@/modules/landing/components/LandingFeatures";
 import { LandingMainContent } from "@/modules/landing/components/LandingMainContent";
 import { LandingFAQ } from "@/modules/landing/components/LandingFAQ";
@@ -9,7 +8,6 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <LandingHero />
-      {/* <LandingFeaturedLogos /> */}
       <LandingFeatures />
       <LandingMainContent />
       <LandingFAQ />
